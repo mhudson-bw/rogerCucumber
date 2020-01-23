f47ff1b88a9992f8242f4a15d541831f32a107d4
Matthew Hudson <mhudson@bandwidth.com>
2020-01-23 11:43:28 -0500

    all files

--- rogerTesting/src/test/java/rogerTesting/StepDefinitions.java
+++ rogerTesting/src/test/java/rogerTesting/StepDefinitions.java
@@ -6,6 +6,37 @@ import io.cucumber.java.en.When;
 
 import static org.junit.Assert.*;
 
+
+class IsItFriday {
+    static String isItFriday(String today) {
+        return "Nope";
+    }
+}
+
 public class StepDefinitions {
+    private String today;
+    private String actualAnswer;
+    private String expectedAnswer;
+
+    @Given("workspace is prepped")
+    public void workspace_is_prepped() {
+        today = "Friday";
+        //throw new io.cucumber.java.PendingException();
+    }
+
+    @When("Master and Slave are on same number")
+    public void master_and_Slave_are_on_same_number() {
+        actualAnswer = IsItFriday.isItFriday(today);
+        //throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("replication is complete")
+    public void replication_is_complete() {
+        expectedAnswer = "Nope";
+        System.out.println(expectedAnswer);
+        assertEquals(expectedAnswer, actualAnswer);
+    }
 
 }
+
+
